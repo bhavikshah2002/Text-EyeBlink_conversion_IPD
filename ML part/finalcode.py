b3a2aa8c73c4e732adb87f9c8f97c027fe7945a6
joshiyash05 <joshiyash05@gmail.com>
2023-04-04 12:08:08 +0530

    ML part done

---
+++ ML part/finalcode.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+modelapna = load_model('model.h5')
+tokenizerapna = pickle.load(open('finaltoken.pkl', 'rb'))
+vocab_array = np.array(list(tokenizerapna.word_index.keys()))
+def make_predictions(text, n_words):
+    for i in range(n_words):
+        text_tokenize = tokenizerapna.texts_to_sequences([text])
+        text_padding  = tf.keras.preprocessing.sequence.pad_sequences(text_tokenize, maxlen = 49)
+        prediction = np.squeeze(np.argmax(modelapna.predict(text_padding), axis = -1))
+        prediction = str(vocab_array[prediction - 1])
+        text += " " + prediction
+    return text
+
+print(make_predictions('hate',2))
